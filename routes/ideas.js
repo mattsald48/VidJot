40e8c5cc4934a2f3d4f0f80617dd0493a656f8cf
mattsald48 <matt.saldana8@gmail.com>
2018-06-07 21:41:56 -0500

    First commit

---
+++ routes/ideas.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
+
+//Load Idea Model
+require('../models/Idea');
+const Idea = mongoose.model('ideas');
+
+//Idea Index Page
+router.get('/', ensureAuthenticated, (req, res) => {
+    Idea.find({user: req.user.id})
+    .sort({date: 'desc'})
+    .then(ideas => {
+      res.render('ideas/index', {
+          ideas:ideas
+      });
+    });
+});
+
+//Add Idea Form
+router.get('/add', ensureAuthenticated, (req, res) => {
+    res.render('ideas/add');
+});
+
+//Edit Idea Form
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        if(idea.user != req.user.id){
+            req.flash('error_msg', 'Not Authorized');
+            res.redirect('/ideas');
+        } else {
+            res.render('ideas/edit', {
+            idea:idea
+            });
+        }
+    });
+});
+
+//Process Form
+router.post('/', ensureAuthenticated, (req, res) => {
+    let errors = [];
+
+    //Checks to see if there is a title and details entered
+    if(!req.body.title){
+        errors.push({text: "Please add a title"});
+    }
+
+    if(!req.body.details){
+        errors.push({text: "Please add some details"});
+    }
+
+    if(errors.length > 0){
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details,
+            user: req.user.id
+        }
+        new Idea(newUser)
+          .save()
+          .then(idea => {
+            req.flash('success_msg', "Video Idea Added");
+              res.redirect('/ideas');
+          })
+    }
+});
+
+//Edit Form process
+router.put('/:id', ensureAuthenticated, (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        //new values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+
+        idea.save()
+            .then(idea => {
+                req.flash('success_msg', "Video Idea Updated");
+                res.redirect('/ideas')
+            })
+    });
+});
+
+//Delete Ideas
+router.delete('/:id', ensureAuthenticated, (req, res) => {
+    Idea.findOneAndRemove({
+        _id: req.params.id,
+        user: req.user.id
+    }).then(() => {
+        req.flash('success_msg', "Video Idea Removed");
+        res.redirect('/ideas');
+    }).catch(err => {
+        req.flash('error_msg', err);
+        res.redirect('/ideas');
+    });
+});
+
+
+
+module.exports = router;
